5ad9aaedcb1ab6bee655df2847f2399d5ab61ec6
AngelEscudero4 <angel.escudero4@hotmail.com>
2023-07-21 21:35:18 +0200

    Basic structure (without camera)

--- app/src/main/java/com/speakinghands/MainActivity2.kt
+++ app/src/main/java/com/speakinghands/MainActivity2.kt
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
     }
 
-    fun onClickBotonIrAVideo(view: View) {
-        val intent = Intent(this, MainActivity2::class.java)
+    fun onClickBotonIrAtras(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
 }
