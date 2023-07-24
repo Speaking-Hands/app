629cfcd84bc4b2ca3cc2ceed41b0a8b794239b1d
AngelEscudero4 <angel.escudero4@hotmail.com>
2023-07-24 17:52:12 +0200

    Un estilo distinto

--- app/src/main/java/com/speakinghands/MainActivity.kt
+++ app/src/main/java/com/speakinghands/MainActivity.kt
@@ -6,31 +6,36 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.VideoView
 import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var button: Button
+    private lateinit var grabarButton: Button
     private lateinit var videoView: VideoView
+    private lateinit var description: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button = findViewById(R.id.button)
+        grabarButton = findViewById(R.id.button)
         videoView = findViewById(R.id.videoView)
+        description = findViewById(R.id.appDescription)
 
-        button.isEnabled = false
+        grabarButton.isEnabled = false
+        videoView.visibility = View.INVISIBLE
 
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 111)
         } else {
-            button.isEnabled = true
+            grabarButton.isEnabled = true
         }
 
-        button.setOnClickListener {
+        grabarButton.setOnClickListener {
             val i = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
             startActivityForResult(i, 1111)
         }
@@ -40,6 +45,10 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if(requestCode == 1111){
+            videoView.visibility = View.VISIBLE
+            description.visibility = View.INVISIBLE
+            grabarButton.visibility = View.INVISIBLE
+
             videoView.setVideoURI(data?.data)
             videoView.start()
         }
@@ -53,7 +62,7 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
         if(requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-            button.isEnabled = true
+            grabarButton.isEnabled = true
         }
     }
 }
