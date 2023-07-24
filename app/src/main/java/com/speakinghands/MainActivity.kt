42087a73bd3ad4d935f811fbd5dbecfa138b4b10
AngelEscudero4 <angel.escudero4@hotmail.com>
2023-07-24 17:07:23 +0200

    Al menos funciona la camara

--- app/src/main/java/com/speakinghands/MainActivity.kt
+++ app/src/main/java/com/speakinghands/MainActivity.kt
@@ -1,18 +1,59 @@
 package com.speakinghands
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.VideoView
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button
+    private lateinit var videoView: VideoView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+        videoView = findViewById(R.id.videoView)
+
+        button.isEnabled = false
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 111)
+        } else {
+            button.isEnabled = true
+        }
+
+        button.setOnClickListener {
+            val i = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
+            startActivityForResult(i, 1111)
+        }
     }
 
-    fun onClickBotonIrAVideo(view: View) {
-        val intent = Intent(this, MainActivity2::class.java)
-        startActivity(intent)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 1111){
+            videoView.setVideoURI(data?.data)
+            videoView.start()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            button.isEnabled = true
+        }
     }
 }
